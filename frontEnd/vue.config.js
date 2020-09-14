9b177c7ca4240be92200bc56f1c19ede81b3e06a
shiweishiwei <969530671@qq.com>
2020-09-14 10:46:43 +0800

    调整端口号，解决跨域问题

--- frontEnd/vue.config.js
+++ frontEnd/vue.config.js
@@ -102,7 +102,7 @@ const vueConfig = {
 
   devServer: {
     // development server port 8000
-    port: 8080,
+    port: 8000,
     // If you want to turn on the proxy, please remove the mockjs /src/main.jsL11
     proxy: {
       '/test': {
